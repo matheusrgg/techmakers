b3dd98b676f838dfd4227166d42b62bff1c4bbb8
Roberto Santos <45983900+betobrasil@users.noreply.github.com>
2019-11-12 10:27:48 -0300

    Alinhamento dos campos para ficar responsivo.

--- PHP_techmakers/novo_usuario.php
+++ PHP_techmakers/novo_usuario.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html>
 
 <head>
@@ -6,40 +7,33 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
   <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+  <link rel="stylesheet" href="css/wireframe.css">
+  <link rel="icon" href="imagens/icone_pagina1.jpg">
   <title> TechMakers </title>
 </head>
 
-<body class="">
-  <nav class="navbar navbar-expand-md navbar-light">
-    <div class="container">
-      <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar6">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbar6">
-        <!--- Início do Logotipo --->
-        <a class="navbar-brand text-primary d-none d-md-block" href="index.php">
-          <img src="imagens/TechMakers_logo.jpeg" width="30" height="30" class="d-inline-block align-top" alt="">
-          <b class="text-success"> TechMakers</b>
-          <!--- Fim do Logotipo --->
-        </a>
-      </div>
-    </div>
-  </nav>
-  <div class="text-center">
+<body>
+
+    <?php
+        include_once("header.php");
+    ?>
+
+   <div class="text-center">
     <div class="container-fluid">
       <div class="row justify-content-center">
         <nav>
           <div class="nav-wrapper"></div>
         </nav>
       </div>
-      <div class="row">
+
+      <div class="row text-center">
         <div class="col-md-12">
           <h1 class="text-success mt-4"><b>Novo Cadastro</b></h1>
         </div>
       </div>
     </div>
   </div>
-  <div class="py-3 text-center">
+  <div class="py-5 mb-5 mr-2 ml-2 text-center">
     <div class="container">
       <div class="row">
         <div class="col-md-12 p-4 border rounded border-success">
@@ -62,13 +56,22 @@
             </div>
             <!-- <button type="submit" class="btn btn-success btn-lg w-25 mr-5">Enviar</button>
                  <button type="submit" class="btn btn-success btn-lg w-25 ml-5">Cancelar</button>   -->
-            <a class="btn btn-success w-25 btn-lg mt-4" href="index.php" role="button">Enviar</a>
-            <a class="btn btn-success w-25 btn-lg ml-5 mt-4" href="index.php" role="button">Cancelar</a>
+            <div class="row">
+                <a class="btn btn-success col-lg-4 col-md-4 col-sm-1 mt-2 m-auto" href="index.php" role="button">Enviar</a>
+                <div class="espaco my-4"></div>
+                <a class="btn btn-success col-lg-4 col-md-4 col-sm-1 mt-2 m-auto" href="index.php" role="button">Cancelar</a>
+            </div>
           </form>
         </div>
       </div>
     </div>
   </div>
+
+      <?php
+        include_once("footer.php");
+      ?>
+
+
   </body>
 
 </html>
