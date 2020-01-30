e269f5463cc328484f3203ae81013517d715259b
Roberto Santos <45983900+betobrasil@users.noreply.github.com>
2020-01-30 10:25:25 -0300

    Inclusão dos campos: ofertas e status na migration

--- BackEnd/database/migrations/2020_01_28_210219_create_produtos_table.php
+++ BackEnd/database/migrations/2020_01_28_210219_create_produtos_table.php
@@ -14,44 +14,34 @@ class CreateProdutosTable extends Migration
     public function up()
     {
         Schema::create('produtos', function (Blueprint $table) {
-            // $table->bigIncrements('id'); 
-            // $table->string('foto', 200);           
-            // $table->string('nome', 50);
-            // $table->string('descricao', 300);
-            // $table->float('preco',10,2); 
-            // $table->string('categoria', 10);
-            // $table->integer('quantidade',50);
-            // $table->boolean('oferta');            
-            // $table->boolean('gamer');
-            // $table->boolean('pessoal1');
-            // $table->boolean('pessoal2');
-            // $table->boolean('profissional1');
-            // $table->boolean('profissional2');
-            // $table->boolean('profissional3');
-            // $table->boolean('profissional4');
-            // $table->boolean('status');
-            // $table->unsignedBigInteger('user_id');
-            // $table->foreign('user_id')->references('id')->on('users');
-            // $table->timestamps();
-
-
             $table->bigIncrements('id');
             $table->string('foto', 200); 
             $table->string('name', 300);
             $table->string('description',300);
             $table->integer('quantity');
+            $table->string('categoria',20);
             $table->float('price',10,2);
-
+            $table->tinyInteger('oferta')->nullable();            
+            $table->tinyInteger('gamer')->nullable();
+            $table->tinyInteger('pessoal1')->nullable();
+            $table->tinyInteger('pessoal2')->nullable();
+            $table->tinyInteger('profissional1')->nullable();
+            $table->tinyInteger('profissional2')->nullable();
+            $table->tinyInteger('profissional3')->nullable();
+            $table->tinyInteger('profissional4')->nullable();
+            $table->tinyInteger('desenvolvedor_ios')->nullable();
+            $table->tinyInteger('desenvolvedor_geral')->nullable();
             $table->boolean('status');
-
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
 
         });
 
+   //produtos::insert(['foto' => '/imagens/02_computacaoDesktop.jpg', 'name' => 'Computador Evolution Pro', 'description' => 'Excelente performace e permite várias conexões com outras plataformas de multimidia, possui mega processador de 35 ghz.', 'quantity' => '5', 'categoria' => 'desktop', 'price' => '13480,00', 'oferta' => '1', 'gamer' => '0', 'pessoal_entretenimento' => '0', 'pessoal_outro' => '0', 'profissional_escritorio' => '0', 'profissional_computacao_grafica' => '1', 'desenvolvedor_ios' => '0', 'desenvolvedor_geral' => '0', 'status' => '1']);         
 
-        // produtos::insert([
+
+        //    produtos::insert([
         //     ['foto' => '/imagens/02_computacaoDesktop.jpg'],    
         //     ['nome' => 'Computador Evolution Pro'],
         //     ['descricao' => 'Excelente performace e permite várias conexões com outras plataformas de multimidia, possui mega processador de 35 ghz.'],
