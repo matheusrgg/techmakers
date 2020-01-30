4bea25345c30a564c11002090b04ffde98989e6f
Roberto Santos <45983900+betobrasil@users.noreply.github.com>
2020-01-30 00:07:06 -0300

    Inclusão dos Controllers

--- BackEnd/app/Http/Controllers/acessoriosController.php
+++ BackEnd/app/Http/Controllers/acessoriosController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Produto;
+
+
 class acessoriosController extends Controller
 {
     public function viewAcessorios(request $REQUEST){
-        return view('acessorios');
+
+          $produtos = Produto::all();   
+     //   $produtos = produtos::where('categoria', '=', 'acessorio')->get();
+
+        return view('acessorios',["produtos"=>$produtos]);
     }
 }
